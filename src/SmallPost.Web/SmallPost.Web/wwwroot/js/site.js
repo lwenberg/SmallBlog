7ef5256dbd54ade263d71c7f0c2c19467c69f23f
Vargtimmen <lwenberg29@gmail.com>
2025-11-11 11:04:55 -0300

    Add real time counter to Create view.

--- src/SmallPost.Web/SmallPost.Web/wwwroot/js/site.js
+++ src/SmallPost.Web/SmallPost.Web/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const messageInputElem = document.getElementById('messageInput');
+const charCountElem = document.getElementById('counter');
+
+messageInputElem.addEventListener('input', function () {
+    const maxLength = this.maxLength;
+    const currentLength = this.value.length;
+    const remainingChars = maxLength - currentLength;
+    charCountElem.textContent = remainingChars + '/'+maxLength;
+});
