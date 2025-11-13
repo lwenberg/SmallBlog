a552d9c42393e782a7a84b90e302538a9024c032
Gerson Molina <lwenberg29@gmail.com>
2025-11-13 13:15:52 -0300

    Add service extension in Infrastructure layer and clean up Program startup.

---
+++ src/SmallPost.Infrastructure/InfrastrcutureServiceExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Infrastructure.Repositories.BlogRespository;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SmallPost.Domain.Services.BlogService;
+
+namespace SmallPost.Infrastructure
+{
+    public static class InfrastrcutureServiceExtensions
+    {
+        public static IServiceCollection AddInfrastructureDBContext(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<WebContext>(options =>
+                    options.UseSqlServer(configuration.GetConnectionString("BlogContext")
+                    ?? throw new InvalidOperationException("Connection string 'MvcMovieContext' not found.")));
+
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<WebContext>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddInfrastructureDependeyGroup(this IServiceCollection services) 
+        {
+            services.AddScoped<IBlogRepository, BlogRepository>();
+
+            return services;
+        }
+    }
+}
